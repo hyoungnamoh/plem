477d9a18446f642440c1003b1094c7700b54c79b
hyoungnam <zzzsh789@naver.com>
2023-01-08 03:31:36 +0900

    Add: 헤더 타이틀, 버튼 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
 import { useNavigation } from '@react-navigation/native';
-import { Image, Pressable, StyleSheet, View } from 'react-native';
+import { Image, Pressable, PressableProps, StyleSheet, View } from 'react-native';
+import PlemText from '../Atoms/PlemText';
 
 type HeaderProps = {
   onBack?: () => void;
   close?: boolean;
+  title?: string;
+  buttonName?: string;
+  buttonProps?: PressableProps;
 };
 
 const backImage = require('../../assets/images/top_ic_back.png');
@@ -13,8 +17,14 @@ const Header = (props: HeaderProps) => {
   const navigation = useNavigation();
   return (
     <View style={styles.header}>
-      <Pressable onPress={props.onBack || navigation.goBack}>
-        <Image source={props.close ? closeImage : backImage} />
+      <View style={styles.left}>
+        <Pressable onPress={props.onBack || navigation.goBack}>
+          <Image source={props.close ? closeImage : backImage} />
+        </Pressable>
+        <PlemText style={styles.title}>{props.title}</PlemText>
+      </View>
+      <Pressable hitSlop={5} {...props.buttonProps}>
+        <PlemText>{props.buttonName}</PlemText>
       </Pressable>
     </View>
   );
@@ -24,7 +34,17 @@ const styles = StyleSheet.create({
   header: {
     paddingHorizontal: 20,
     height: 60,
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  left: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginLeft: 12,
   },
 });
 
