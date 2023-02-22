3d7aeb94422c538c0c14da0905ed95b249b4797c
hyoungnam <zzzsh789@naver.com>
2023-02-23 00:27:42 +0900

    Add: 스케줄 반복 설정 페이지 UI 작업 완료

--- src/pages/ScheduleRepeatSettingPage/index.tsx
+++ src/pages/ScheduleRepeatSettingPage/index.tsx
@@ -3,88 +3,51 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { useEffect, useState } from 'react';
 import { Image, Pressable, StyleSheet, View } from 'react-native';
 import { useRecoilState } from 'recoil';
-import { AddPlanChart, PlanChart, Repeats } from '../../../types/chart';
-import { DaysOfWeek, DaysOfWeekKor } from '../../../types/date';
+import { AddSchedule, Repeat } from '../../../types/calendar';
 import PlemText from '../../components/Atoms/PlemText';
 import BottomButton from '../../components/BottomButton';
 import Header from '../../components/Header';
 import { MAIN_COLOR } from '../../constants/color';
-import { addPlanChartState } from '../../states/addPlanChartState';
+import { addScheduleState } from '../../states/addScheduleState';
 import { CalendarTabStackParamList } from '../../tabs/CalendarTab';
-import { MainTabStackParamList } from '../../tabs/MainTab';
 
-type RepeatOptionKor =
-  | '안 함'
-  | '월요일마다'
-  | '화요일마다'
-  | '수요일마다'
-  | '목요일마다'
-  | '금요일마다'
-  | '토요일마다'
-  | '일요일마다'
-  | '날짜 지정';
+type RepeatOptionKor = '안 함' | '매일' | '화요일마다' | '매주' | '2주마다' | '매월' | '매년' | '사용자화';
 
-export type RepeatOptionItem = {
-  key: DaysOfWeek | 'off' | 'days';
-  value: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
-  label: RepeatOptionKor;
-  day?: DaysOfWeekKor;
-};
-
-export const repeatOptionList: RepeatOptionItem[] = [
-  { key: 'off', value: 0, label: '안 함' },
-  { key: 'mon', value: 1, label: '월요일마다', day: '월' },
-  { key: 'tue', value: 2, label: '화요일마다', day: '화' },
-  { key: 'wed', value: 3, label: '수요일마다', day: '수' },
-  { key: 'thu', value: 4, label: '목요일마다', day: '목' },
-  { key: 'fri', value: 5, label: '금요일마다', day: '금' },
-  { key: 'sat', value: 6, label: '토요일마다', day: '토' },
-  { key: 'sun', value: 7, label: '일요일마다', day: '일' },
-  { key: 'days', value: 8, label: '날짜 지정' },
-];
+export const repeatOptionList: readonly { value: Repeat; label: RepeatOptionKor }[] = [
+  { value: null, label: '안 함' },
+  { value: 'every', label: '매일' },
+  { value: 'week', label: '매주' },
+  { value: '2weeks', label: '2주마다' },
+  { value: 'month', label: '매월' },
+  { value: 'year', label: '매년' },
+  { value: 'custom', label: '사용자화' },
+] as const;
 
 type RepeatSettingPageProps = NativeStackScreenProps<CalendarTabStackParamList, 'ScheduleRepeatSettingPage'>;
 
 const checkImage = require('../../assets/images/check.png');
 
 const ScheduleRepeatSettingPage = ({ navigation }: RepeatSettingPageProps) => {
-  const [chart, setChart] = useRecoilState(addPlanChartState);
-  const [repeatOptions, setRepeatOptions] = useState<Repeats>(chart.repeats);
+  const [schedule, setSchedule] = useRecoilState(addScheduleState);
+  const [repeatOption, setRepeatOption] = useState<Repeat>(schedule.repeat);
 
   useEffect(() => {}, []);
 
-  const onPressRepeatOption = (option: DaysOfWeek | 'off' | 'days') => {
-    if (option === 'off') {
-      setRepeatOptions(['off']);
-      return;
+  const onPressRepeatOption = (option: Repeat) => {
+    setRepeatOption(option);
+    if (option === 'custom') {
+      navigation.navigate('RepeatCustomSettingPage');
     }
-    if (option === 'days') {
-      setRepeatOptions(['days']);
-      // navigation.navigate('SelectRepeatDatePage');
-      return;
-    }
-    if (repeatOptions.includes(option)) {
-      const copiedOptions = [...repeatOptions];
-      const index = copiedOptions.findIndex((e) => e === option);
-      copiedOptions.splice(index, 1);
-      setRepeatOptions(copiedOptions);
-      return;
-    }
-    setRepeatOptions(repeatOptions.concat([option]).filter((e) => !(e === 'days' || e === 'off')));
   };
 
-  const setStorageChartData = async (chartData: AddPlanChart) => {
+  const setStorageChartData = async (chartData: AddSchedule) => {
     await AsyncStorage.setItem('chart_data', JSON.stringify(chartData));
   };
 
   const onPressBottomButton = () => {
-    const copiedChart = { ...chart };
-    copiedChart.repeats = repeatOptions;
-    if (!repeatOptions.includes('days')) {
-      copiedChart.repeatDays = [];
-    }
-    setChart(copiedChart);
-    setStorageChartData(copiedChart);
+    const copiedSchedule = { ...schedule };
+    copiedSchedule.repeat = repeatOption;
+    setSchedule(copiedSchedule);
 
     navigation.goBack();
   };
@@ -95,17 +58,12 @@ const ScheduleRepeatSettingPage = ({ navigation }: RepeatSettingPageProps) => {
       <View style={styles.listWrap}>
         {repeatOptionList.map((repeat) => {
           return (
-            <Pressable key={repeat.key} style={styles.listItem} onPress={() => onPressRepeatOption(repeat.key)}>
+            <Pressable key={repeat.value} style={styles.listItem} onPress={() => onPressRepeatOption(repeat.value)}>
               <PlemText>{repeat.label}</PlemText>
-              {repeatOptions.includes(repeat.key) && <Image source={checkImage} />}
+              {repeatOption === repeat.value && <Image source={checkImage} />}
             </Pressable>
           );
         })}
-        <View style={styles.infoMessageWrap}>
-          {repeatOptions.includes('days') && chart.repeatDays.length > 0 && (
-            <PlemText style={styles.infoMessage}>매월 {chart.repeatDays.join('일, ')}일 마다 반복됩니다</PlemText>
-          )}
-        </View>
       </View>
       <BottomButton title={'완료'} onPress={onPressBottomButton} />
     </View>
--- types/calendar.d.ts
+++ types/calendar.d.ts
@@ -2,16 +2,20 @@ export type Schedule = {
   id: number;
   name: string;
   category: string;
-  repeats: Repeats;
+  repeat: Repeat;
   createdAt: string;
   updatedAt: null;
   startTime: Date;
   endTime: Date;
-  notification: PlanNotification;
+  notification: ScheduleNotification;
   repeatDays: number[];
 };
+
+export type ScheduleNotification = 0 | 1 | 5 | 10 | 15 | 30 | 60;
 
 export type AddSchedule = Omit<Schedule, 'createdAt' | 'id' | 'updatedAt'>;
 
 export type Category = 'daily' | 'birth' | 'promise' | 'banking';
 export type CategoryKor = '일상' | '생일' | '약속' | '금융';
+
+export type Repeat = null | 'every' | 'week' | '2weeks' | 'month' | 'year' | 'custom';
