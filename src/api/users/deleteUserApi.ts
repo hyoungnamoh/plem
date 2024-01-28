79414d37f4a3388bc3652139b2d46b38c1964157
hyoungnam <zzzsh789@naver.com>
2024-01-28 18:09:37 +0900

    Add: deleteUserApi, mutation 추가

---
+++ src/api/users/deleteUserApi.ts
@@ -0,0 +1,15 @@
+import apiRequest from '..';
+import { ApiResponse } from '../../../types/axios';
+
+export type DeleteUserBody = {
+  password: string;
+};
+
+export type DeleteUserResponse = {
+  id: number;
+};
+
+export const deleteUserApi = async (body: DeleteUserBody) => {
+  const response = await apiRequest.delete<ApiResponse<DeleteUserResponse>>('/users', { data: body });
+  return response.data;
+};
