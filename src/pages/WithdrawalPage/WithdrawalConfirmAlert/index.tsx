d0c076013130a1b0ca1dab564e73f43f9c703a92
hyoungnam <zzzsh789@naver.com>
2024-02-25 15:25:54 +0900

    Fix: 계정 삭제 확인 얼럿 텍스트 중앙정렬

--- src/pages/WithdrawalPage/WithdrawalConfirmAlert/index.tsx
+++ src/pages/WithdrawalPage/WithdrawalConfirmAlert/index.tsx
@@ -4,7 +4,7 @@ import PlemAlert, { PlemAlertProps } from 'components/PlemAlert/PlemAlert';
 const WithdrawalConfirmAlert = (props: Omit<PlemAlertProps, 'children' | 'size'>) => {
   return (
     <PlemAlert size="small" {...props}>
-      <PlemText style={{ alignItems: 'center' }}>
+      <PlemText style={{ alignItems: 'center', textAlign: 'center' }}>
         {'계정을 삭제할까요?\n삭제 시 모든 데이터는 복구할 수 없어요.'}
       </PlemText>
     </PlemAlert>
