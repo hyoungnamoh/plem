b8986ad1cd2b661b07a737a442158dac9db111dc
hyoungnam <zzzsh789@naver.com>
2024-04-28 03:40:26 +0900

    Modify: codepush checkFrequency 옵션 수정

--- src/utils/configCodepush.ts
+++ src/utils/configCodepush.ts
@@ -1,7 +1,7 @@
 import CodePush, { CodePushOptions } from 'react-native-code-push';
 
 export const codePushOptions: CodePushOptions = {
-  checkFrequency: CodePush.CheckFrequency.MANUAL, // 지정한 때에 업데이트
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME, // 백그라운드에서 앱으로 돌아올 때마다 실행
   installMode: CodePush.InstallMode.IMMEDIATE,
   mandatoryInstallMode: CodePush.InstallMode.IMMEDIATE,
 };
