7efcd83772ee92a355e6e945d3f011015bbed444
hyoungnam <zzzsh789@naver.com>
2024-05-01 23:06:42 +0900

    Add: 공지사항 알림 눌렀을 때 공지사항 페이지로 이동되는 기능 추가

--- src/components/NavigationWrapper/index.tsx
+++ src/components/NavigationWrapper/index.tsx
@@ -3,6 +3,10 @@ import { MAIN_COLOR } from 'constants/colors';
 import { Dispatch, ReactNode, SetStateAction } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { bottomSafeAreaState } from 'states/bottomSafeAreaState';
+import { Linking } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+
+const NAVIGATION_IDS = ['home', 'post', 'noticeListPage'];
 
 const NavigationWrapper = ({
   children,
@@ -13,6 +17,76 @@ const NavigationWrapper = ({
 }) => {
   const navigationRef = createNavigationContainerRef();
   const setBottomSafeArea = useSetRecoilState(bottomSafeAreaState);
+
+  function buildDeepLinkFromNotificationData(data: any): string | null {
+    const navigationId = data?.navigationId;
+    if (!NAVIGATION_IDS.includes(navigationId)) {
+      console.warn('Unverified navigationId', navigationId);
+      return null;
+    }
+    if (navigationId === 'home') {
+      return 'plem://home';
+    }
+    if (navigationId === 'noticeListPage') {
+      return 'plem://noticeListPage';
+    }
+    const postId = data?.postId;
+    if (typeof postId === 'string') {
+      return `plem://post/${postId}`;
+    }
+    console.warn('Missing postId');
+    return null;
+  }
+
+  const linking = {
+    prefixes: ['plem://'],
+    config: {
+      screens: {
+        Home: 'home',
+        Post: 'post/:id',
+        SettingTab: {
+          screens: {
+            NoticeListPage: 'noticeListPage',
+          },
+        },
+      },
+    },
+    async getInitialURL() {
+      const url = await Linking.getInitialURL();
+
+      if (typeof url === 'string') {
+        return url;
+      }
+      //getInitialNotification: When the application is opened from a quit state.
+      const message = await messaging().getInitialNotification();
+      const deeplinkURL = buildDeepLinkFromNotificationData(message?.data);
+
+      if (typeof deeplinkURL === 'string') {
+        return deeplinkURL;
+      }
+    },
+    subscribe(listener: (url: string) => void) {
+      const onReceiveURL = ({ url }: { url: string }) => {
+        listener(url);
+      };
+      // Listen to incoming links from deep linking
+      const linkingSubscription = Linking.addEventListener('url', onReceiveURL);
+
+      // 앱 꺼져있을 때, 백그라운드에 있을 때 노티온 거 눌렀을 때 실행
+      const unsubscribe = messaging().onNotificationOpenedApp((remoteMessage) => {
+        const url = buildDeepLinkFromNotificationData(remoteMessage.data);
+
+        if (typeof url === 'string') {
+          listener(url);
+        }
+      });
+
+      return () => {
+        linkingSubscription.remove();
+        unsubscribe();
+      };
+    },
+  };
 
   return (
     <NavigationContainer
@@ -31,7 +105,8 @@ const NavigationWrapper = ({
           const currentRouteName = currentRoute.name;
           setRouteName(currentRouteName);
         }
-      }}>
+      }}
+      linking={linking}>
       {children}
     </NavigationContainer>
   );
